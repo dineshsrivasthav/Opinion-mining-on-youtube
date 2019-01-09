d6f7331f0e6d31db86fefca3799d19a3378b15c1
dineshsrivasthav <46534373+dineshsrivasthav@users.noreply.github.com>
2019-01-09 22:38:09 +0530

    Update pro.r

--- pro.r
+++ pro.r
@@ -1,13 +1,11 @@
-#"5eDqRysaico" ,sadg-"bM9BXu9lPZ0", america-"SVick42IZAk",23c-"M52IPSJDXfg"
-
 library(vosonSML)
-apikey <- "AIzaSyBooGhuGkl_eR63hTNWfysPh8bARtrZoP8"
+apikey <- "#API-KEY" #put your api key in the quotes
 key <- AuthenticateWithYoutubeAPI(apikey)
 
-video <- c('bM9BXu9lPZ0')  ###open j.csv file for bM9BXu9lPZ0
+video <- c('video-link')  #put the url-id of the youtube video in the quotes
 ytdata <- CollectDataYoutube(video, key, writeToFile = FALSE)
 str(ytdata)
-write.csv(ytdata, file='~/yt.csv', row.names = F)
+write.csv(ytdata, file='##location of the csv file containing the list of commenters with the reply count', row.names = F)
 data <-read.csv(file.choose(), header = T)
 str(data)
 
@@ -42,17 +40,17 @@ comments[3]
 barplot(100*colSums(s)/sum(s), las = 2, col = rainbow(10), ylab = 'Percentage', main = 'Sentiment scores for Youtube comments')
 
 
-#small world
-sw <- sample_smallworld(dim=1.5, size=126, nei=3, p=0.1)
-#plot(sw, vertex.size=6, vertex.label=NA, layout=layout_in_circle)
+#small world model
+# sw <- sample_smallworld(dim=1.5, size=126, nei=3, p=0.1)
+# plot(sw, vertex.size=6, vertex.label=NA, layout=layout_in_circle)
 
-#random graph
-er <- sample_gnm(n=126, m=200) 
-er1 <- sample_gnp(n=126, p=0.1) 
-#plot(er, vertex.size=6, vertex.label=NA)
-#plot(er1, vertex.size=6, vertex.label=NA)
-#dbinom(135,7875,0.1)
+#random graph model
+# er <- sample_gnm(n=126, m=200) 
+# er1 <- sample_gnp(n=126, p=0.1) 
+# plot(er, vertex.size=6, vertex.label=NA)
+# plot(er1, vertex.size=6, vertex.label=NA)
+# dbinom(135,7875,0.1)
 
 #preferential attachment model
-ba <-  sample_pa(n=126, power=1, m=1,  directed=F)
-#plot(ba, vertex.size=2, vertex.label=NA)
+# ba <-  sample_pa(n=126, power=1, m=1,  directed=F)
+# plot(ba, vertex.size=2, vertex.label=NA)
